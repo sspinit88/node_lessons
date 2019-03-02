4b61dbb6a4807127d83046edac49db4212f69922
sspinit88 <sspinit88@gmail.com>
2019-03-02 16:05:13 +0300

    added path, html, css

--- 4-node_lesson/app.js
+++ 4-node_lesson/app.js
@@ -1,6 +1,23 @@
-const http = require('http');
-const routes = require('./routes');
+// next(); - обеспечивает переход к следуюшей функции
+// res.send(); -  отправка ответа
+// body-parser - модуль, обрабатывает тела запросов и выставляет для них req.body
+// urlencoded - именно с помощью этого метода и анализируется тело
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const server = http.createServer(routes);
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// 404 и страница не найдена
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
